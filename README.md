3eb324b725ff4dabacc4caf062d7d145feb2a0ce
ramorais <ramorais@gmail.com>
2020-02-05 13:28:02 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DM - Servers.json README
+
+Copia o arquivo local de servidores (servers.json) para a pasta '.totvsls' do usuário. A extensão é ativada apenas se encontrar o arquivo 'servers.json' na pasta '.vscode' do projeto corrente.
+
---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode';
+import { copyFile } from 'fs';
+import { URI } from 'vscode-uri';
+
+// ---
+// Localização dos arquivos
+const homedirFile = require('os').homedir() + "\\.totvsls\\servers.json";
+let localFile = "";
+// ---
+const fs = require('fs');
+
+// ---
+// activate()
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "Local Servers" is now active!');
+
+	// Pasta local de trabalho
+	var folders = vscode.workspace.workspaceFolders ?? null;
+	if(folders !== null) {
+		if (folders.length > 0) { 
+			localFile = folders[0].uri.fsPath + "\\.vscode\\servers.json";		
+		}
+	}
+
+	swapFile();	// Troca os arquivos...
+
+}
+// -----------------------------------------------------------------------------
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+// ---
+// Copia o arquivo local para a pasta
+// vista pelo TDS
+function swapFile() {
+	let exist = fs.existsSync(localFile);
+	if (exist) {
+		vscode.window.showInformationMessage('Servers.json: Transferindo...');
+		try {
+			vscode.workspace.fs.copy(URI.file(localFile), URI.file(homedirFile), {overwrite: true});	
+		} catch (error) {
+			
+		}
+		
+		vscode.window.showInformationMessage('Servers.json: OK :-)');
+	}
+}
+// -----------------------------------------------------------------------------
+
